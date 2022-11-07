ecea02accf30e5ac5dd704037f01b599a397d193
poiu <94011063+poiudfg@users.noreply.github.com>
2022-11-07 14:21:15 +0700

    Add files via upload

---
+++ optimal_pages.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+df = pd.read_csv('PR1.csv' ,header=None).to_numpy()
+
+pages = []
+for pnum in list(df):
+    pages.append(pnum)
+
+totality_pages = len(pages)
+frams = []
+total_frams = []
+page_falt = []
+num_falt = 0
+print("Enter the number of frames: ",end="")
+#num_pages =  int(input())
+num_pages = 3
+check_optimal = [None for i in range(num_pages)]
+for i in range(totality_pages-1):
+    if pages[i] not in frams:
+        page_falt.append(["Miss"])
+        num_falt+=1
+        if len(frams) < num_pages:
+            frams.append(pages[i])
+        else:
+            for j in range(len(frams)):
+                if frams[j] not in pages[i+1:]:
+                    frams[j] = pages[i]
+                    break
+                else:
+                    check_optimal[j] = pages[i+1:].index(frams[j])
+            if pages[i] not in frams:
+                frams[check_optimal.index(max(check_optimal))] = pages[i]
+        total_frams.append([])
+        for t in list(frams):
+            total_frams[-1].append(t)
+    else:
+        page_falt.append(["Hit"])
+        total_frams.append([])
+        for t in list(frams):
+            total_frams[-1].append(t)
+
+for x in range(totality_pages-1):
+    print(pages[x] , end="")
+    print(page_falt[x], end="")
+    for p in total_frams[x]:
+        print(p, end="")
+    print("")
+print()
+print(num_falt , end="")
+print(" pages falut")
